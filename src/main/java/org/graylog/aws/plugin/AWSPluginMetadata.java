829d9f8fdb50b363e237d877c652902b287a0d97
Lennart Koopmann <lennart@graylog.com>
2017-03-09 17:53:36 -0600

    update version in metadata

--- src/main/java/org/graylog/aws/plugin/AWSPluginMetadata.java
+++ src/main/java/org/graylog/aws/plugin/AWSPluginMetadata.java
@@ -32,7 +32,7 @@ public class AWSPluginMetadata implements PluginMetaData {
 
     @Override
     public Version getVersion() {
-        return Version.from(1, 2, 2);
+        return Version.from(1, 3, 0);
     }
 
     @Override
