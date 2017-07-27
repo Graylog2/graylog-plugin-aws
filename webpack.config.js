7b434ea2bafac4d1868b791871f6214915fc2971
Bernd Ahlers <bernd@graylog.com>
2017-07-27 21:35:48 +0200

    Fix copy & paste error in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,6 @@ const loadBuildConfig = require('graylog-web-plugin').loadBuildConfig;
 const path = require('path');
 
 // Remember to use the same name here and in `getUniqueId()` in the java MetaData class
-module.exports = new PluginWebpackConfig('org.graylog.plugins.auth.sso.SsoAuthPlugin', loadBuildConfig(path.resolve(__dirname, './build.config')), {
+module.exports = new PluginWebpackConfig('org.graylog.aws.plugin.AWSPlugin', loadBuildConfig(path.resolve(__dirname, './build.config')), {
   // Here goes your additional webpack configuration.
 });
