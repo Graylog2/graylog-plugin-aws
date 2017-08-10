5c4c36ab6f3e2cf24ad73fe258ac82cac085c7cd
Bernd Ahlers <bernd@graylog.com>
2017-08-10 13:13:28 +0200

    Add payload examples and ignore unknwon properties

--- src/main/java/org/graylog/aws/cloudwatch/CloudWatchLogEvent.java
+++ src/main/java/org/graylog/aws/cloudwatch/CloudWatchLogEvent.java
@@ -1,8 +1,22 @@
 package org.graylog.aws.cloudwatch;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.MoreObjects;
 
+/**
+ * A single CloudWatch log event.
+ * <p/>
+ * Example payload:
+ * <pre>
+ * {
+ *   "id": "33503748002479370955346306650196094071913271643270021120",
+ *   "timestamp": 1502360020000,
+ *   "message": "2 123456789 eni-aaaaaaaa 10.0.27.226 10.42.96.199 3604 17720 17 1 132 1502360020 1502360079 REJECT OK"
+ * }
+ * </pre>
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CloudWatchLogEvent {
     @JsonProperty("timestamp")
     public long timestamp;
