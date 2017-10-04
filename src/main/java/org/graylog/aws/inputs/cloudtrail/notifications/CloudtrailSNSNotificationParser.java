58a0449921a66e162901df9ce4a247bac1cd7be6
Jochen Schalanda <jochen@graylog.com>
2017-10-04 11:40:56 +0200

    Prevent failing on unknown JSON properties in CloudtrailSNSNotificationParser
    The Jackson ObjectMapper used in CloudtrailSNSNotificationParser was configured to
    fail on unknown properties (default) and thus parsing the SNS notifications failed
    if the format was changed in AWS.
    
    Fixes #44

--- src/main/java/org/graylog/aws/inputs/cloudtrail/notifications/CloudtrailSNSNotificationParser.java
+++ src/main/java/org/graylog/aws/inputs/cloudtrail/notifications/CloudtrailSNSNotificationParser.java
@@ -1,42 +1,42 @@
 package org.graylog.aws.inputs.cloudtrail.notifications;
 
 import com.amazonaws.services.sqs.model.Message;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.collect.Lists;
 import org.graylog.aws.inputs.cloudtrail.json.CloudtrailWriteNotification;
 import org.graylog.aws.json.SQSMessage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class CloudtrailSNSNotificationParser {
-    private final ObjectMapper om;
-
-    public CloudtrailSNSNotificationParser() {
-        om = new ObjectMapper();
-    }
+    private final ObjectMapper objectMapper = new ObjectMapper().disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 
     public List<CloudtrailSNSNotification> parse(Message message) {
-        List<CloudtrailSNSNotification> notifications = Lists.newArrayList();
 
         try {
-            SQSMessage envelope = om.readValue(message.getBody(), SQSMessage.class);
+            final SQSMessage envelope = objectMapper.readValue(message.getBody(), SQSMessage.class);
 
             if (envelope.message == null) {
                 return Collections.emptyList();
             }
 
-            CloudtrailWriteNotification notification = om.readValue(envelope.message, CloudtrailWriteNotification.class);
+            final CloudtrailWriteNotification notification = objectMapper.readValue(envelope.message, CloudtrailWriteNotification.class);
 
-            for (String s3ObjectKey : notification.s3ObjectKey) {
+            final List<String> s3ObjectKeys = notification.s3ObjectKey;
+            if (s3ObjectKeys == null) {
+                return Collections.emptyList();
+            }
+
+            final List<CloudtrailSNSNotification> notifications = new ArrayList<>(s3ObjectKeys.size());
+            for (String s3ObjectKey : s3ObjectKeys) {
                 notifications.add(new CloudtrailSNSNotification(message.getReceiptHandle(), notification.s3Bucket, s3ObjectKey));
             }
+            return notifications;
         } catch (IOException e) {
             throw new RuntimeException("Could not parse SNS notification: " + message.getBody(), e);
         }
-
-        return notifications;
     }
-
 }
