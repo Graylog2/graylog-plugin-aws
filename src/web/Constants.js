f579223757900c1acef5e8f84864b7bc1a8c00c7
Dennis Oelkers <dennis@graylog.com>
2020-05-06 16:11:11 +0200

    Using custom REST endpoint in AWS configlet.

---
+++ src/web/Constants.js
@@ -0,0 +1,1 @@
+export const PLUGIN_PACKAGE = 'org.graylog.aws.config.AWSPluginConfiguration';
--- src/web/index.jsx
+++ src/web/index.jsx
@@ -3,12 +3,13 @@ import 'webpack-entry';
 import { PluginManifest, PluginStore } from 'graylog-web-plugin/plugin';
 import AWSPluginsConfig from 'components/AWSPluginConfiguration';
 import packageJson from '../../package.json';
+import { PLUGIN_PACKAGE } from './Constants';
 
 PluginStore.register(new PluginManifest(packageJson, {
   systemConfigurations: [
     {
       component: AWSPluginsConfig,
-      configType: 'org.graylog.aws.config.AWSPluginConfiguration',
+      configType: PLUGIN_PACKAGE,
     },
   ],
 }));
